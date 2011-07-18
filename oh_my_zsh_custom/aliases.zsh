d7b2d8bd05c67d48352b758ba3359037206d8403
Gavin Heavyside <gavin@heavyside.co.uk>
2011-07-18 08:14:26 +0100

    adding zsh/oh-my-zsh dotfiles and customisations

---
+++ oh_my_zsh_custom/aliases.zsh
@@ -0,0 +1,28 @@
+alias ls="ls -FG"
+alias ll="ls -FGal"
+alias lart="ls -FGlart"
+
+alias office="ssh -t office.mydrivesolutions.com ssh typhoon"
+alias harrier="ssh -t office.mydrivesolutions.com ssh harrier"
+alias citation="ssh -Y 192.168.0.100"
+alias typhoon="ssh -Y 192.168.0.104"
+
+alias emacs="/Applications/Emacs.app/Contents/MacOS/Emacs"
+alias emacsclient="/Applications/Emacs.app/Contents/MacOS/bin/emacsclient -n"
+
+alias ldd="otool -L"
+
+alias spec="spec --color"
+alias rspec="rspec --color"
+
+alias s="screen -RAad"
+
+alias start_postgres="pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start"
+alias stop_postgres="pg_ctl -D /usr/local/var/postgres stop -s -m fast"
+
+alias vi="mvim"
+alias vim="mvim"
+
+alias top=htop
+
+alias lazytest="java -cp \"src:test:classes:lib/*:lib/dev/*\" lazytest.watch src test"
---
+++ oh_my_zsh_custom/exports.zsh
@@ -0,0 +1,11 @@
+export PATH=$PATH:$HOME/bin
+
+export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Home"
+export EC2_HOME=`brew --prefix ec2-api-tools`/jars
+
+export JAVA_OPTS=-Xmx768m
+
+export OPSCODE_USER=hgavin
+export ORGNAME=mydrive
+
+[[ -s $HOME/.ec2/mydrive/access_keys ]] && source $HOME/.ec2/mydrive/access_keys
